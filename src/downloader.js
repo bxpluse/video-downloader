cb08f5cd1a22b742d451105e41cdde7ca1f6cfee
Zopyrion <21994296+Zopyrion@users.noreply.github.com>
2020-07-12 20:45:50 -0400

    Release version 1.0.0

--- src/downloader.js
+++ src/downloader.js
@@ -1,4 +1,32 @@
-function getData(){
-    console.log('Called!!!');
-    alert("logged");
+const exec = require('child_process').exec;
+const app = require('electron').remote.app
+
+
+function download(url){
+
+    // Options to pass into command
+    const options = {
+        '-o' : getPath() + '/%(title)s'
+    }
+
+    let command = 'youtube-dl ';
+
+    for (const [key, value] of Object.entries(options)) {
+        command += key + " " + value + " ";
+    }
+    command += url
+
+    const process = exec(command, {cwd: './libs'}) // Send command to cmd
+
+    // Stream output to textarea viewable in html
+    process.stdout.on('data', function(data) {
+        console.log(data);
+    });
+
+}
+
+function getPath(){
+    // Get path of app
+    const path = app.getAppPath().toString()
+    return path.replace(/\\/g, "/");
 }
